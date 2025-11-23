c7af3fe0997ce790f0c6d8f5ae79c76e03bab8e2
Jaired Jawed <me@jairedjawed.com>
2025-11-23 08:39:03 -0800

    fix: use getServerSideProps to fix homepage 404
    Refactored the homepage to use `getServerSideProps` to fetch data on the server-side. This fixes a 404 error that was occurring in production.
    
    Also, configured ESLint to run non-interactively and removed the old ESLint config file.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { getFirestore, collection, getCountFromServer, query, where, limit, getDocs } from 'firebase/firestore'
 import { getFirebaseApp } from '../utils/firebase.config'
 import seedRandom from 'seedrandom'
 import { useRouter } from 'next/router'
 import { saveAs } from 'file-saver'
 import Button from '../components/button'
+import { GetServerSideProps } from 'next'
 
 type Caption = {
   text: string
@@ -19,73 +20,10 @@ type Meme = {
   memeUrl: string
 }
 
-export default function RenderMeme() {
+export default function RenderMeme({ meme, error }: { meme: Meme | null, error: string | null}) {
   const router = useRouter()
 
-  const [isLoading, setLoadingStatus] = useState<boolean>(true)
-  const [error, setError] = useState<string | null>(null)
-  const [meme, setMemeData] = useState<Meme | null>(null)
-
-  useEffect(() => {
-    const { app } = getFirebaseApp()
-    const db = getFirestore(app)
-
-    async function getMeme() {
-      const coll = collection(db, "memes");
-      const memeSnapshot = await getCountFromServer(coll);
-    
-      // generate a random number with seedrandom because Math.random() didn't feel random enough
-      const randomString = (Math.random() + 1).toString(36).substring(8)
-      const rng = seedRandom(randomString)
-      const randomIndex = Math.floor(rng() * memeSnapshot.data().count)
-
-      const randomMemeQuery = query(collection(db, 'memes'), where("random", "==", randomIndex), limit(1))
-      const meme = await getDocs(randomMemeQuery)
-
-      if (meme.docs.length === 0) {
-        setError('No meme could be found.')
-        setLoadingStatus(false)
-      }
-
-      else {
-        const memeData = meme.docs[0].data()
-
-        // find any hastaags within the invitation and give them lightblue color in dark mode
-        // and a regular shade of blue in light mode
-        const invitation = memeData.invitation.replace(/(^|\W)(#.*?(?= #|$))/ig, '$1<span class="text-blue-600 dark:text-blue-400">$2</span>')
-
-        // generate meme image from imgflip
-        const imgFlipMemeId = memeData.imgFlipMemeId
-        const captions = memeData.captions.map((caption: Caption) => caption.text)
-        const response = await fetch(`/api/memes/images/create/${imgFlipMemeId}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ captions }),
-        })
-
-        if (!response.ok) {
-          const { error } = await response.json()
-          setError(error)
-        }
-
-        const { memeUrl } = await response.json()
-
-        setMemeData({
-          captions: memeData.captions,
-          imgFlipMemeId: memeData.imgFlipMemeId,
-          created: memeData.created,
-          invitation,
-          memeUrl,
-        })
-
-        setLoadingStatus(false)
-      }
-    }
-
-    getMeme()
-  }, [router])
+  const [isLoading, setLoadingStatus] = useState<boolean>(false)
 
   if (isLoading) {
     return null;
@@ -127,5 +65,87 @@ export default function RenderMeme() {
     return (
       <div>Something went wrong</div>
     )
+  }
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { app } = getFirebaseApp()
+  const db = getFirestore(app)
+
+  try {
+    const coll = collection(db, "memes");
+    const memeSnapshot = await getCountFromServer(coll);
+  
+    // generate a random number with seedrandom because Math.random() didn't feel random enough
+    const randomString = (Math.random() + 1).toString(36).substring(8)
+    const rng = seedRandom(randomString)
+    const randomIndex = Math.floor(rng() * memeSnapshot.data().count)
+
+    const randomMemeQuery = query(collection(db, 'memes'), where("random", "==", randomIndex), limit(1))
+    const meme = await getDocs(randomMemeQuery)
+
+    if (meme.docs.length === 0) {
+      return {
+        props: {
+          meme: null,
+          error: 'No meme could be found.'
+        }
+      }
+    }
+
+    else {
+      const memeData = meme.docs[0].data()
+
+      // find any hastaags within the invitation and give them lightblue color in dark mode
+      // and a regular shade of blue in light mode
+      const invitation = memeData.invitation.replace(/(^|\W)(#.*?(?= #|$))/ig, '$1<span class="text-blue-600 dark:text-blue-400">$2</span>')
+
+      // generate meme image from imgflip
+      const imgFlipMemeId = memeData.imgFlipMemeId
+      const captions = memeData.captions.map((caption: Caption) => caption.text)
+      const protocol = context.req.headers['x-forwarded-proto'] || 'http'
+      const host = context.req.headers.host
+      const response = await fetch(`${protocol}://${host}/api/memes/images/create/${imgFlipMemeId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ captions }),
+      })
+
+      if (!response.ok) {
+        const { error } = await response.json()
+        return {
+          props: {
+            meme: null,
+            error
+          }
+        }
+      }
+
+      const { memeUrl } = await response.json()
+
+      return {
+        props: {
+          meme: {
+            captions: memeData.captions,
+            imgFlipMemeId: memeData.imgFlipMemeId,
+            created: memeData.created.toDate().toISOString(),
+            invitation,
+            memeUrl,
+          },
+          error: null
+        }
+      }
+    }
+  }
+
+  catch (e: any) {
+    return {
+      props: {
+        meme: null,
+        error: e.message
+      }
+    }
   }
 }
